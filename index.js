c69da553801dbfe8abe0c5a6fa9e2c52a1c640ec
Akrammallah <153257896+Akrammallah@users.noreply.github.com>
2024-03-23 16:15:37 +0500

    This is my guess number game project

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const guessNum = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([{
+        name: "guessNumberGame",
+        type: "number",
+        message: "guess the number",
+    }]);
+if (answer.guessNumberGame === guessNum) {
+    console.log("Congratulation you guess right number");
+}
+else {
+    console.log("you guess wrong number");
+}
