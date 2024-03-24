4e5e5803b07301305ce195342e6632316042cff8
Akrammallah <153257896+Akrammallah@users.noreply.github.com>
2024-03-24 14:27:34 +0500

    change name

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 const guessNum = Math.floor(Math.random() * 6 + 1);
 const answer = await inquirer.prompt([{
         name: "guessNumberGame",
         type: "number",
-        message: "guess the number",
+        message: "guess the number:",
     }]);
 if (answer.guessNumberGame === guessNum) {
     console.log("Congratulation you guess right number");
